fe97bdff7174fe0a7c5f11411df82994ebcb5ce3
Serg_Nout <kuchukov.sergey@gmail.com>
2024-09-28 09:12:51 +0300

    add

---
+++ functions.py
@@ -0,0 +1,18 @@
+def func(input_json):
+    input_json = ''
+    return  '''
+    [
+        {
+            "date": "2021-05-01",
+            "amount": 1000,
+            "currency": "USD",
+            "description": "Salary"
+        },
+        {
+            "date": "2021-05-03",
+            "amount": -20,
+            "currency": "USD",
+            "description": "Coffee"
+        }
+    ]
+    '''
---
+++ tests/test_11_12_5_2.py
@@ -0,0 +1,56 @@
+import pytest
+from functions import func
+
+
+@pytest.fixture
+def input_json():
+    return  '''[
+    {
+        "date": "2021-05-01",
+        "amount": 1000,
+        "currency": "USD",
+        "description": "Salary"
+    },
+    {
+        "date": "2021-05-02",
+        "amount": -50,
+        "currency": "EUR",
+        "description": "Dinner"
+    },
+    {
+        "date": "2021-05-03",
+        "amount": -20,
+        "currency": "USD",
+        "description": "Coffee"
+    },
+    {
+        "date": "2021-05-04",
+        "amount": 200,
+        "currency": "GBP",
+        "description": "Gift"
+    }
+    ]'''
+@pytest.fixture
+def output_json():
+    return  '''
+    [
+        {
+            "date": "2021-05-01",
+            "amount": 1000,
+            "currency": "USD",
+            "description": "Salary"
+        },
+        {
+            "date": "2021-05-03",
+            "amount": -20,
+            "currency": "USD",
+            "description": "Coffee"
+        }
+    ]
+    '''
+
+
+def test_func(input_json: str, output_json: str) -> None:
+    result = func(input_json)
+    assert result  == output_json
+    pass
